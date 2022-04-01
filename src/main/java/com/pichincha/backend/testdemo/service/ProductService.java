0f7333fd641caf3a5182d941269ed3267895e4ca
jennymolina <jennyal19@hotmail.com>
2022-03-31 23:32:17 -0500

    second commit

---
+++ src/main/java/com/pichincha/backend/testdemo/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.pichincha.backend.testdemo.service;
+
+import com.pichincha.backend.testdemo.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+}
